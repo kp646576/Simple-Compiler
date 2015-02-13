9c46a3e77380f8d7bb00329ddbf0bfc8ccca950f
kp646576 <kp70726F@gmail.com>
2015-02-13 03:10:49 -0800

    Fixed initial production and added notes.

--- parser.py
+++ parser.py
@@ -1,51 +1,64 @@
 from token import Token
 from lexer import Lexer
 from parseTree import ParseTree
+import sys
 
 class Parser:
     def __init__(self, lexer):
         self.tokens = lexer.tokens
-        self.curToken = ''
+        self.curToken = Token(0, '', '')
         self.peekToken = ''
         if len(self.tokens) > 1:
             self.curToken = self.tokens[0]
             self.peekToken = self.tokens[1]
         self.tokenIdx = 1
+        self.depth = 0
+        self.tab = '    '
         #self.curNode = ParseNode(Token(0, 'root', 'root'))
-        self.S()
+        self.S(0)
         #self.parseTree = ParseTree(self.currentNode)
 
-#S   -> (S'S'' | exprS''
-#S'  -> ) | S)
-#S'' -> SS'' | epsilon
-    def S(self):
+    #S   -> (S'S'' | exprS''
+    #S'  -> )      | S)
+    #S'' -> SS''   | epsilon
+    def S(self, d):
         # (S'S''
         if self.curToken and self.curToken.value == '(':
-            print 'S  : ('
+            #print 'S  : ('
+            print d * self.tab + '('
             self.getNextToken()
-            self.SP()
-            self.SPP()
+            self.SP(d)
+            self.SPP(d)
         #else:
             # go to expr
             #self.SPP()
+        else:
+            self.error()
 
-    def SP(self):
+    #S'  -> ) | S)
+    def SP(self, d):
         # S)
         if self.curToken and self.curToken.value != ')':
-            self.S()
+            print 'goes in'
+            self.S(d + 1)
         # S) and )
-        if self.curToken.value == ')':
-            print 'SP : )'
+        if not self.curToken:
+            self.error()
+        elif self.curToken.value == ')':
+            #print 'SP : )'
+            print d * self.tab + ')'
             self.getNextToken()
 
     # S'' -> SS'' | epsilon
-    def SPP(self):
+    def SPP(self, d):
         # SS''
         if self.curToken and self.curToken.value == '(': # or is expr
         #or S production?# why do I need? or self.isTerminalType():
-            print 'SPP: '
-            self.S()
-            self.SPP()
+            #print 'SPP: '
+            self.S(d)
+            self.SPP(d)
+        elif self.curToken and self.curToken.value == ')':
+            self.error()
         # epsilon
 
     def expr(self):
@@ -86,7 +99,7 @@ class Parser:
 
 
     def error(self):
-        print >> sys.stderr,"Parser error on line " + str(self.curToken.line) + ' with token value ' + str(self.curToken.value) + ' token index ' + str(self.tokenIdx)
+        print >> sys.stderr,"Parser error line: " + str(self.curToken.line) + ' token value: ' + str(self.curToken.value) + ' token index: ' + str(self.tokenIdx)
 
     def getNextToken(self):
         self.curToken = self.peekToken
@@ -95,4 +108,4 @@ class Parser:
             self.peekToken = self.tokens[self.tokenIdx]
         else:
             #print "goes in"
-            self.peekToken = ''
+            self.peekToken = Token(self.curToken.line, '', '')
